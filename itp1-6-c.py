99009b766401a3fe27c0141aaa7b29252923ea98
nama.Pit <nama.Pit@gmail.com>
2018-09-20 00:11:46 +0900

    2018/09/19 ITP1_6_Cを途中まで書いた

--- itp1-6-c.py
+++ itp1-6-c.py
@@ -26,3 +26,33 @@ while True:
 
 
 # あとは書き出すための部分を書く。
+
+# =========================================================================
+
+n_room = 10     # 1フロアの部屋の数
+n_floor = 3     # 1棟ごとの階数
+n_blds = 4      # 棟の数
+cnt1 = 0           # 配列作成時のカウンタ
+
+
+while True:
+    ary[cnt1] = [[0] * n_room for i in range(n_floor)]
+    cnt1+=1
+    if cnt1 == n_blds:
+        break
+    
+
+n = int(input())    # データ数を取得
+cnt2 = 0            # データ取得用カウンタの初期化
+
+while True:
+    b,f,r,v = map(int, input().split())
+    ary[b-1][f-1][r-1] += v
+    cnt2 += 1
+    if cnt2 == n:
+        break
+    
+
+cnt_b = 0       # 棟を回すためのカウンタ
+cnt_f = 0       # 階を回すためのカウンタ
+
